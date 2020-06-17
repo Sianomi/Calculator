5bdaef4fc0561e48336810a1f87f7d3cbffb80c1
Sianomi <65516317+Sianomi@users.noreply.github.com>
2020-06-17 16:34:32 +0900

    mainClass 테스트

---
+++ Calculator/src/main/mainClass.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.Scanner;
+//import java.io.File;
+
+public class mainClass {
+	
+	Scanner sc = new Scanner(System.in);
+	String value;
+	void input()
+	{
+		System.out.println("원하는 번호를 입력해주세요.");
+		System.out.println("1.계산  2.삭제 3.기록조회 0.종료");
+		value = sc.nextLine();
+	}
+	
+	public static void main(String[] args) {
+		mainClass mainObject = new mainClass();
+		while(true)
+		{
+			mainObject.input();
+			if(mainObject.value.equalsIgnoreCase("0")) break;
+			
+			System.out.println(mainObject.value);
+		}
+	}
+
+}
